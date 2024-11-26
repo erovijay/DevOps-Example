4fd1c7aa288bb5b81996c66a1e4230dfa685a3e7
erovijay <erovijay@gmail.com>
2024-11-26 14:29:43 +0530

    committed

---
+++ sam.java
@@ -0,0 +1,13 @@
+ Volume in drive C is Windows 
+ Volume Serial Number is 6A57-1FDF
+
+ Directory of C:\DevOps-Example
+
+26-11-2024  14:28    <DIR>          .
+26-11-2024  14:19               314 .gitignore
+26-11-2024  14:19            35,823 LICENSE
+26-11-2024  14:19                49 README.md
+26-11-2024  14:28                 0 sam.java
+26-11-2024  14:25               433 sample1.txt
+               5 File(s)         36,619 bytes
+               1 Dir(s)  250,816,929,792 bytes free
